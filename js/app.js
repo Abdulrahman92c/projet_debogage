717d8cae8365b089708e8172644c845ac2ca3896
abdulrahman92c <chahdiabdulrahman@gmail.com>
2022-09-29 13:45:03 +0200

    modif du CSS

--- js/app.js
+++ js/app.js
@@ -1,14 +1,10 @@
-let img = document.querySelector('img');
-let text = document.querySelector('.toggle-pannel');
-for(i = 0;i<img.length;i++){
-    let Itms = new ToggleItem(img[i], text[i]);
-}
-
+const img = document.querySelector('img');
+const text = document.querySelector('.toggle-pannel');
 
 let isVisible =  false;
 console.log(isVisible);
 img.addEventListener('click', ()=>{
             isVisible = !isVisible;
             isVisible ? text.classList.add('is-visible'): text.classList.remove('is-visible');
-            
+            console.log(isVisible);
 });
