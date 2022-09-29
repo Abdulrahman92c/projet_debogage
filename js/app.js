59e3494e4a9934dbea127847c3607ccb543c1611
abdulrahman92c <chahdiabdulrahman@gmail.com>
2022-09-29 16:29:57 +0200

    update CSS

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", e => {
-const img = document.querySelector('.visible-pannel');
-const text = document.querySelector('.toggle-pannel');
+    const img = document.querySelector('.visible-pannel');
+    const text = document.querySelector('.toggle-pannel');
 
 
-let isVisible =  false;
-console.log(isVisible);
-img.addEventListener('click', ()=>{
+    let isVisible =  false;
+    console.log(isVisible);
+    img.addEventListener('click', ()=>{
             isVisible = !isVisible;
             isVisible ? text.classList.add('is-visible'): text.classList.remove('is-visible');
             console.log(isVisible);
-});
+    });
 });
